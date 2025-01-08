258a7e1b55ed352b9eb81ced4df8436676cc4de5
MarkHiarley <hiarleycostamarley@gmail.com>
2025-01-07 21:40:43 -0300

    atualizacao de codigo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var child_process_1 = require("child_process");
 function downloadVideo(url) {
-    var command = "yt-dlp -f b ".concat(url);
+    var command = "yt-dlp -f bestvideo ".concat(url);
     //yt-dlp -f bestaudio
     //yt-dlp -f bestvideo
     (0, child_process_1.exec)(command, function (error, stdout, stderr) {
@@ -17,5 +17,5 @@ function downloadVideo(url) {
         console.log("stdout: ".concat(stdout));
     });
 }
-var videoUrl = 'https://www.youtube.com/watch?v=o2qFATSNdu4';
+var videoUrl = 'https://youtu.be/VH3TKI0V9Zc?si=TXGj1HwPxXzSM-05';
 downloadVideo(videoUrl);
--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import { exec } from 'child_process';
-
 function downloadVideo(url: string) {
-    const command = `yt-dlp -f b ${url}`;
+    const command = `yt-dlp -f bestvideo ${url}`;
     //yt-dlp -f bestaudio
     //yt-dlp -f bestvideo
 
+
+    
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Erro ao baixar o vídeo: ${error.message}`);
@@ -17,6 +18,5 @@ function downloadVideo(url: string) {
         console.log(`stdout: ${stdout}`);
     });
 }
-
-const videoUrl = 'https://www.youtube.com/watch?v=o2qFATSNdu4';
+const videoUrl = 'https://youtu.be/rC7Y04k5Tbo?si=7voBvKQZioR5Pj1m';
 downloadVideo(videoUrl);
